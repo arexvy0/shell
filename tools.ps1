ca1b58fe54c64fe88607069b20dd34c859b0a0eb
arexvy0 <adriangrepl1337@web.de>
2024-10-10 15:05:36 +0200

    tools.ps1

--- tools.ps1
+++ tools.ps1
@@ -1,10 +1,8 @@
-Write-Host "Bitte wählen Sie eine der folgenden Optionen, um ein Programm herunterzuladen:"
-Write-Host "1) RegestryChangesView"
-Write-Host "2) USBDriveLog"
-Write-Host "3) ShellExView"
-Write-Host "4) JumpList"
-Write-Host "5) USBForensicLookup"
-Write-Host ""
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+$downloadFolder = "$env:USERPROFILE\Desktop\Pc Check"
+if (-not (Test-Path -Path $downloadFolder)) {
+    New-Item -ItemType Directory -Path $downloadFolder | Out-Null
+}
 
 $urls = @{
     "1" = "https://www.nirsoft.net/utils/registrychangesview-x64.zip"
@@ -12,10 +10,31 @@ $urls = @{
     "3" = "https://www.nirsoft.net/utils/shexview-x64.zip"
     "4" = "https://www.nirsoft.net/utils/jumplistsview.zip"
     "5" = "https://e5hforensics.com/wp-content/uploads/2023/03/USB-Forensic-Tracker-v1.1.3.zip"
+    "6" = "https://www.nirsoft.net/utils/dnsdataview.zip"
 }
 
-$downloadFolder = "$env:USERPROFILE\Desktop\Pc Check"
+while ($true) {
+    Write-Host "Bitte wählen Sie eine der folgenden Optionen, um ein Programm herunterzuladen:"
+    Write-Host "1) RegistryChangesView"
+    Write-Host "2) USBDriveLog"
+    Write-Host "3) ShellExView"
+    Write-Host "4) JumpList"
+    Write-Host "5) USBForensicLookup"
+    Write-Host "6) DNSDataView"
+    Write-Host "q) Beenden"
 
-if (-not (Test-Path -Path $downloadFolder)) {
-    New-Item -ItemType Directory -Path $downloadFolder | Out-Null
+    $selection = Read-Host "Ihre Auswahl"
+
+    if ($selection -eq "q") {
+        break
+    } elseif ($urls.ContainsKey($selection)) {
+        $url = $urls[$selection]
+        $fileName = Split-Path $url -Leaf
+        $outputPath = Join-Path $downloadFolder $fileName
+        Write-Host "Herunterladen von $fileName..."
+        Invoke-WebRequest -Uri $url -OutFile $outputPath
+        Write-Host "Download abgeschlossen: $outputPath"
+    } else {
+        Write-Host "Ungültige Auswahl. Bitte erneut versuchen."
+    }
 }
