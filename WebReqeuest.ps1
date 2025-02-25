70d1fe48d8f72fdba59f9160dc942eecd8394d2e
arexvy0 <adriangrepl1337@web.de>
2025-02-25 09:09:04 +0100

    Kii

--- WebReqeuest.ps1
+++
@@ -1,7 +0,0 @@
-$downloadFolder = "$env:USERPROFILE\Downloads"
-$output = Join-Path $downloadFolder "tools.ps1"
-$url = "https://raw.githubusercontent.com/arexvy0/shell/main/tools.ps1"
-
-Invoke-WebRequest -Uri $url -OutFile $output
-
-Write-Host "Datei wurde erfolgreich heruntergeladen: $output"
